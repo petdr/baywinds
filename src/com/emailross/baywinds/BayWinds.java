5e758962661845c2682bdb34c78582bf5f0721fe
Peter Ross <peter@emailross.com>
2010-11-05 16:49:00 +1100

    Document URIS containing forecast and observations for the bay.

--- src/com/emailross/baywinds/BayWinds.java
+++ src/com/emailross/baywinds/BayWinds.java
@@ -3,6 +3,11 @@ package com.emailross.baywinds;
 import android.app.Activity;
 import android.os.Bundle;
 
+// Latest BOM melbourne observations
+// ftp://ftp2.bom.gov.au/anon/gen/fwo/IDV60034.html
+
+// Latest Melbourne forecast
+// ftp://ftp2.bom.gov.au/anon/gen/fwo/IDV10460.xml
 public class BayWinds extends Activity
 {
     /** Called when the activity is first created. */
