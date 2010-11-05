1a02c3d9d6805fce9b03f5142a9cc32d4198d6ea
Peter Ross <peter@emailross.com>
2010-11-05 16:21:56 +1100

    Initial project creation.
    The following command-line was used to target android 2.1 with google apis
    
    android create project -n baywinds -t 11 -p ./baywinds -k com.emailross.baywinds -a BayWinds

---
+++ src/com/emailross/baywinds/BayWinds.java
@@ -0,0 +1,15 @@
+package com.emailross.baywinds;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BayWinds extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
